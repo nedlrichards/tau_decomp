92284ba23ecc1576710907a07bc058585dfa0470
Ned Richards <edwardlrichards@gmail.com>
2022-07-27 18:34:29 -0700

    moving through draft

--- reports/jasa/dynamic_prof.py
+++ reports/jasa/dynamic_prof.py
@@ -38,7 +38,7 @@ ax[2].plot(c_field[:, prof_i] - 1500., z_a[plt_i])
 #[a.set_prop_cycle(None) for a in ax]
 #ax[0].plot([-10, 100], [sld_z[prof_i], sld_z[prof_i]], alpha=0.6)
 #ax[1].plot([-10, 100], [sld_z[prof_i], sld_z[prof_i]], alpha=0.6)
-ax[2].plot([-10, 1e4], [sld_z[prof_i], sld_z[prof_i]], color='0.6')
+ax[2].plot([-10, 1e4], [sld_z[prof_i], sld_z[prof_i]], color='0.4')
 
 ax[0].set_ylabel('Depth (m)')
 ax[0].set_xlabel('$\sigma_0$ (kg/m$^3$)')
@@ -87,3 +87,8 @@ ax[2].set_position(pos)
 
 fig.savefig(join(savedir, 'sld_profile.png'), dpi=300)
 
+# compute gradient in mixed layer
+z_i = z_a[plt_i] < sld_z[prof_i[-1]]
+y = c_field[z_i, :][:, prof_i[-1]]
+fit = linregress(z_a[plt_i][z_i], y=y)
+ax[2].plot(fit.intercept + z_a * fit.slope - 1500., z_a)
