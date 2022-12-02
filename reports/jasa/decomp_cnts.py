82a1ab595bfd67ecf5ac0df27aa4281d050baab3
Ned Richards <edwardlrichards@gmail.com>
2022-12-01 21:24:38 -0800

    new draft

--- reports/jasa/decomp_cnts.py
+++ reports/jasa/decomp_cnts.py
@@ -24,75 +24,46 @@ x_a = inputed['x_a']
 #max_x = 250
 #call_lvls = [39, 48, 54]
 
-min_x = 175
-max_x = 400
-call_lvls = [38, 42, 48]
+min_x = 230
+max_x = 350
+call_lvls = [34, 38, 42, 48, 52]
 
 #min_x = 200
 #max_x = 400
 #call_lvls = [38, 41, 50]
 
-fig, ax = plt.subplots(2, 1, sharex=True, figsize=(cf.jasa_1clm, 3))
+fig, ax = plt.subplots(sharex=True, figsize=(cf.jasa_1clm, 2))
 x_i = x_a < max_x * 1e3
 
-cc0 = ['0.6', '0.4', '0.2']
-z_off = [-10, -5, 0]
+cc0 = ['0.6', '0.5', '0.4', '0.3', '0.2']
+z_off = [60, 0, 0, 10, 20]
 for zo, c0, lbl_i in zip(z_off, cc0, call_lvls):
     plt_height = stable_lvls[0, lbl_i, :]
     plt_inds = plt_height > 1e-5
-    ax[0].plot(x_a[plt_inds] / 1e3, plt_height[plt_inds], color='#be0119', alpha=0.6)
-    ax[0].plot(x_a / 1e3, lvls[0, lbl_i, :].T, color=c0)
+    ax.plot(x_a[plt_inds] / 1e3, plt_height[plt_inds], color='#be0119', alpha=0.6, linewidth=1)
+    plt_lvls = lvls[0, lbl_i, :]
+    plt_lvls[np.isnan(plt_lvls)] = -2
+    ax.plot(x_a / 1e3, plt_lvls, color=c0)
 
-    ax[0].text(max_x + 3., lvls[0, lbl_i, x_i][-1] + zo,
+    ax.text(max_x + 5., lvls[0, lbl_i, x_i][-1] + zo,
                f'{sigma[lbl_i]:.2f}', va='center', color=c0)
 
-ax[0].set_ylim(130, 0)
-ax[0].set_xlim(min_x, max_x)
+ax.set_ylim(130, 0)
+ax.set_xlim(min_x, max_x)
+ax.set_xticks([230, 250, 270, 290, 310, 330])
+ax.set_xticks([240, 260, 280, 300, 320, 340], minor=True)
+ax.xaxis.set_ticks_position('both')
 
-pos = ax[0].get_position()
+pos = ax.get_position()
 pos.x1 -= 0.07
-pos.x0 += 0.10
+pos.x0 += 0.08
 pos.y1 += 0.08
-pos.y0 += 0.06
-ax[0].set_position(pos)
+pos.y0 += 0.13
+ax.set_position(pos)
 
-ax[0].set_ylabel('Depth (m)')
-ax[0].text(max_x - 3., 10, '$\sigma$', va='center')
-ax[0].text(max_x - 3., 30, '(kg/m$^3$)', va='center')
-
-pos = ax[1].get_position()
-pos.x1 -= 0.07
-pos.x0 += 0.10
-pos.y1 += 0.06
-pos.y0 += 0.04
-ax[1].set_position(pos)
-
-ax[1].set_ylabel(r'$\gamma$ (kg/m$^3$)', labelpad=-0.05)
-
-z_off = [-0.02, 0.05, 0.00]
-for zo, c0, lbl_i in zip(z_off, cc0, call_lvls):
-    ax[1].plot(x_a / 1e3, lvls[1, lbl_i, :].T, color=c0)
-    ax[1].text(max_x + 3., lvls[1, lbl_i, x_i][-1] + zo,
-               f'{sigma[lbl_i]:.2f}', color=c0, va='center')
-
-ax[1].plot(x_a / 1e3, stable_lvls[1, call_lvls[-1], :],color='#be0119', alpha=0.6)
-
-ax[1].set_xlabel('Range (km)')
-
-ax[1].set_ylim(-0.3, 0.35)
-
-ax[0].text(min_x - 25., 5, '(a)', bbox=cf.bbox)
-ax[1].text(min_x - 25., 0.35, '(b)', bbox=cf.bbox)
-
-
-ax[0].spines["right"].set_visible(False)
-ax[0].spines["top"].set_visible(False)
-#ax[0].xaxis.set_ticks_position('bottom')
-#ax[0].yaxis.set_ticks_position('left')
-
-ax[1].spines["right"].set_visible(False)
-ax[1].spines["top"].set_visible(False)
-#ax[1].xaxis.set_ticks_position('bottom')
-#ax[1].yaxis.set_ticks_position('left')
+ax.set_ylabel('Depth (m)')
+ax.set_xlabel('Transcet range, $x$ (km)')
+ax.text(max_x + 1., 10, '$\sigma$', va='center')
+ax.text(max_x + 1., 30, '(kg/m$^3$)', va='center')
 
 fig.savefig(join(savedir, 'sig_tau_interp.png'), dpi=300)
